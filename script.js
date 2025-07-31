49c30a7ad7bac2ed8b814c2331537081982327b2
STORM533 <sakshamkr2006@gmail.com>
2025-08-01 01:09:49 +0530

    button to add book created some small initial design done.

--- script.js
+++ script.js
@@ -8,10 +8,36 @@ function Book(name , author , pages , status){
     this.pages = pages;
     this.status = status;
     this.info = function() {
-        return this.name + ' by ' + this.author+ ', '  + this.pages  + 'pages ' ;
+        return this.name + ' by ' + this.author+ ', '  + this.pages  + ' pages .' ;
     };
-    
 }
+
+const theHobbit = new Book('adasd','adadaw','adasdad','adwasda');
+const theRedeem = new Book('asdasd','awdasd','adasda','adasd');
+
 function addBookToLibrary() {
+    myLibrary.push(theHobbit.info());
+    myLibrary.push(theRedeem.info());
+}
+console.log(addBookToLibrary());
+console.log(myLibrary);
+console.log(theHobbit.info());
+console.log(theRedeem.info());
+
+function createBooks(){
     
 }
+const container = document.querySelector(".container");
+const books = document.createElement("div");
+books.classList.add("books");
+books.textContent= myLibrary[0];
+container.appendChild(books);
+const button = document.createElement("button");
+button.classList.add("createBook");
+button.textContent  ="ADD BOOKS";
+    button.addEventListener("click" ,() =>{
+
+    });
+    container.appendChild(button);
+    
+
