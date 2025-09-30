6673ac0f30ff28bc1e8eef4a75f5064ec0493f60
STORM533 <146557098+STORM533@users.noreply.github.com>
2025-09-30 18:48:05 +0530

    changed factory function into class

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const myLibrary = [];
-function Book(name , author , pages , status){
-    if(!new.target){
-        throw Error("you must use the 'new' operator to call the constructor");
+class Book {
+    constructor(name, author, pages, status) {
+        if (!new.target) {
+            throw Error("you must use the 'new' operator to call the constructor");
+        }
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+        this.info = function () {
+            return this.name + ' by ' + this.author + ', ' + this.pages + ' pages . status: ' + this.status;
+        };
     }
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.info = function() {
-        return this.name + ' by ' + this.author+ ', '  + this.pages  + ' pages . status: ' + this.status;
-    };
 }
 
 function addBookToLibrary() {
